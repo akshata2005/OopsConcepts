b6b505c6a1a4550028288aa52acc3beb33135775
akshata2005 <akshatasawant1805@gmail.com>
2022-01-14 09:47:52 +0530

    [akshata] Adding inheritance.cs

---
+++ OppsConcepts/Inheritance/Vehicle.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OppsConcepts.Inheritance
+{
+    public class Vehicle
+    {
+        public string brand;
+        public string model;
+        public void start()
+        {
+            Console.WriteLine("vehicle starts from parent");
+        }
+    }
+}
